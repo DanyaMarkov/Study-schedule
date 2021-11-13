758a67458765c1732dbc8e5bb9e7a7be39673494
roqzych <roqzych@gmail.com>
2021-11-14 01:17:49 +0300

    Start work

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+fetch("base.json")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
