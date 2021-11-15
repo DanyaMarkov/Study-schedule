d62b66ea90371fd5d6a8ace427ee18bf5e21ee33
roqzych <roqzych@gmail.com>
2021-11-15 18:41:43 +0300

    Add local storage

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+
+window.onload = function () {
+    let input = localStorage.getItem("group")
+    document.querySelector('.header__inputtext').value = input;
+    console.log("Загрузил значение в инпут:" + input)
+
+    showSchedule();
+};
+
+function Storage() {
+    var currentValue = document.querySelector('.header__inputtext').value;
+    console.log(currentValue)
+    if (currentValue == "") {
+    }
+    else {
+        localStorage.setItem("group", currentValue)
+        document.querySelector('.header__inputtext').value = window.localStorage.getItem('group');
+
+    }
+}
+
 function showSchedule() {
     fetch("base.json")
         .then((response) => response.json())
@@ -12,10 +33,13 @@ function showSchedule() {
             for (let i in data) {
                 if (data[i].groupNumber == val) {
 
-                    let days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+                    let days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 
                     for (let j in days) {
                         let currentDay = days[j];
+                        if (currentDay == "sunday") {
+                            continue;
+                        }
                         for (let k in data[i][currentDay].lessons) {
                             console.log(typeof (data[i][currentDay]))
                             let block = "<div id='time'>" + data[i][currentDay].lessons[k].time + "</div>"
@@ -24,6 +48,15 @@ function showSchedule() {
                                 + "<div id='teacher'>" + data[i][currentDay].lessons[k].teacher + "</div>"
                                 + "<div id='dates'>" + data[i][currentDay].lessons[k].dates + "</div>"
                             document.getElementById(currentDay).innerHTML += "<div class='scheduleBlock'>" + block + "</div>";
+
+                            let d = new Date();
+                            let n = d.getDay();
+
+                            if (days[n] == currentDay) {
+                                document.getElementById(currentDay).style.backgroundColor = "#d2fcd7";
+                            }
+                            //console.log(days[n]);
+
                         }
                         findgroup = true;
                     }
@@ -37,7 +70,6 @@ function showSchedule() {
         .catch((error) => {
             console.log(error);
         });
-
 }
 
 function clearDays() {
